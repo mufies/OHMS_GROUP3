921c956c6b87b663c8daebfa60bbeda330b37e2f
haidang71214 <haidang71214@gmail.com>
2025-09-15 15:57:40 +0700

    update

---
+++ src/main/java/com/example/ohms/OhmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ohms/OhmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
