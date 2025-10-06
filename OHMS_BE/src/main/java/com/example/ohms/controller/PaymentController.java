a9e01378a1eb9e2f63d74a2e5111d66c77511c03
walshiiro <kixsuna1@gmail.com>
2025-10-06 20:19:36 +0700

    update payment

--- OHMS_BE/src/main/java/com/example/ohms/controller/PaymentController.java
+++ OHMS_BE/src/main/java/com/example/ohms/controller/PaymentController.java
@@ -1,5 +1,45 @@
 package com.example.ohms.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ohms.dto.response.ApiResponse;
+import com.example.ohms.dto.response.PaymentRespone;
+import com.example.ohms.service.PaymentService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("${spring.application.api-prefix}/payment")
+@RequiredArgsConstructor
 public class PaymentController {
-    
+    private final PaymentService paymentService;
+    @GetMapping("/vn-pay")
+    public ApiResponse<PaymentRespone> pay(HttpServletRequest request) {
+
+        // return new Ap<>(HttpStatus.OK, "Success", paymentService.createVnPayPayment(request));
+        return ApiResponse.<PaymentRespone>builder()
+        .code(200)
+        .results(paymentService.createVnPayPayment(request))
+        .build();
+    }
+    @GetMapping("/vn-pay-callback")
+    public ApiResponse<PaymentRespone> payCallbackHandler(HttpServletRequest request) {
+        String status = request.getParameter("vnp_ResponseCode");
+        if (status.equals("00")) {
+            return ApiResponse.<PaymentRespone>builder()
+                .code(200)
+                .message("Payment successful")
+                .results(new PaymentRespone("200","ok",""))
+                .build();
+        } else {
+            return ApiResponse.<PaymentRespone>builder()
+                .code(400)
+                .message("Payment failed")
+                .results(new PaymentRespone("200","ok",""))
+                .build();
+        }
+    }
 }
--- OHMS_BE/src/main/java/com/example/ohms/service/PaymentService.java
+++ OHMS_BE/src/main/java/com/example/ohms/service/PaymentService.java
@@ -1,11 +1,47 @@
 package com.example.ohms.service;
 
+import java.util.Map;
 import org.springframework.stereotype.Service;
 
+import com.example.ohms.configuration.PaymentConfig;
+import com.example.ohms.dto.response.PaymentRespone;
+import com.example.ohms.utils.PaymentUtils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PaymentService {
+    private final PaymentConfig vnPayConfig;
+
+    public PaymentRespone createVnPayPayment(HttpServletRequest request) {
+        long amount = Integer.parseInt(request.getParameter("amount")) * 100L;
+        String bankCode = request.getParameter("bankCode");
+        Map<String, String> vnpParamsMap = vnPayConfig.getVNPayConfig();
+        vnpParamsMap.put("vnp_Amount", String.valueOf(amount));
+        if (bankCode != null && !bankCode.isEmpty()) {
+            vnpParamsMap.put("vnp_BankCode", bankCode);
+        }
+        vnpParamsMap.put("vnp_IpAddr", PaymentUtils.getIpAddress(request));
+        //build query url
+        String queryUrl = PaymentUtils.getPaymentURL(vnpParamsMap, true);
+        String hashData = PaymentUtils.getPaymentURL(vnpParamsMap, false);
+
+        // log.info("hashdata: {}",hashData);
+        log.info("secretkey: {}",vnPayConfig.getSecretKey());
+        String vnpSecureHash = PaymentUtils.hmacSHA512(vnPayConfig.getSecretKey(), hashData);
+        log.info("vnpSecueHash: {}",vnpSecureHash);
+        queryUrl += "&vnp_SecureHash=" + vnpSecureHash;
+                log.info("return Url{}",  vnPayConfig.getReturnUrl());
+
+        String paymentUrl = vnPayConfig.getUrl() + "?" + queryUrl;
+        return PaymentRespone.builder()
+                .code("200")
+                .message("success")
+                .paymentUrl(paymentUrl).build();
+    }
     
 }
